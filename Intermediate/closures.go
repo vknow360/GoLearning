1a84352cb2f0aa34fd93902a9cdb8a13aa337404
Sunny Gupta <vknow360@gmail.com>
2025-10-04 23:52:52 +0530

    Implement closure example in closures.go and add .gitignore for .idea directory

--- Intermediate/closures.go
+++ Intermediate/closures.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	sequence := adder()
+	fmt.Println(sequence())
+}
+
+func adder() func() int {
+	i := 0
+	fmt.Println("previous value of i:", i)
+	return func() int {
+		i++
+		fmt.Println("Added 1 to i")
+		return i
+	}
 }
